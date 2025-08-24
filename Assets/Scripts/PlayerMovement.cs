f34bb10e1006e02051671d1f3cfd3b44cd8066e8
Terrence <tmims>
2025-08-24 15:16:06 -0700

    Player can jump now
    Player can jump with spacebar

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,20 +3,22 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Rigidbody2D rb;
+    public Rigidbody2D rb; //player body
 
     public float moveSpeed;
     
-    public float horizontalMove;
+    public bool isGrounded;
+    
+    public float horizontalMove; //float for input in which direction player goes
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         rb.linearVelocity = new Vector2(horizontalMove * moveSpeed, rb.linearVelocity.y);
     }
@@ -24,5 +26,24 @@ public class PlayerMovement : MonoBehaviour
     public void Move(InputAction.CallbackContext context)
     {
         horizontalMove = context.ReadValue<Vector2>().x; 
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        //let player jump if not on ground
+        if (isGrounded)
+        {
+            rb.AddForce(Vector2.up * 10f, ForceMode2D.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            Debug.Log("GROUNDED");
+            isGrounded = true;
+        }
     }
 }
